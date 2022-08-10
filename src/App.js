b6bda746fcf1637a30ea6bf235684ec48c3c9c44
Martin Zeitler <martin@zeitler-it.com>
2022-08-10 18:50:22 +0200

    Implemented routes as in tutorial video, does not work

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Tasks from './components/Tasks';
 import { useState, useEffect } from "react"
 import AddTask from './components/AddTask';
 import Footer from './components/Footer';
+import About from './components/About'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(true)
   const [tasks, setTasks] = useState([])
- //var i = 0
+  //var i = 0
 
   useEffect(() => {
-    const getTasks = async() => {
+    const getTasks = async () => {
       const tasksFromServer = await fetchTasks()
       //if(i > 0){
       //console.log(tasksFromServer)
@@ -22,29 +24,29 @@ function App() {
     getTasks()
   }, [])
 
-  
 
-  const fetchTasks = async() => {
+
+  const fetchTasks = async () => {
     const res = await fetch('http://localhost:2045/tasks')
     const data = await res.json()
     return data
   }
 
-  const fetchTask = async(id) => {
+  const fetchTask = async (id) => {
     const res = await fetch(`http://localhost:2045/tasks/${id}`)
     const data = await res.json()
     return data
   }
-  
-  const addTask = async(task) => {
+
+  const addTask = async (task) => {
     //const id = Math.floor(Math.random() * 1000000)
     //var newTask = {id, ...task}
     //setTasks([...tasks, newTask])
     //console.log(task)
     const res = await fetch('http://localhost:2045/tasks', {
-      method : "POST",
-      headers : {
-        'Content-Type' : "application/json"
+      method: "POST",
+      headers: {
+        'Content-Type': "application/json"
       },
       body: JSON.stringify(task)
     })
@@ -53,15 +55,15 @@ function App() {
     setTasks([...tasks, data])
   }
 
-  const deleteTask = async(id) => {
+  const deleteTask = async (id) => {
     await fetch(`http://localhost:2045/tasks/${id}`, {
-      method : "DELETE"
+      method: "DELETE"
     })
     // console.log('delete', id)
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
-  const switchTaskReminder = async(id) => {
+  const switchTaskReminder = async (id) => {
     // console.log(`Switch for item ${id}`)
     // var item = (tasks.find((task) => task.id = id))
     // var index = tasks.indexOf(item)
@@ -69,15 +71,15 @@ function App() {
     // item.reminder = !item.reminder
     // tasks.splice(index, 1, item)
     const taskToToggle = await fetchTask(id)
-    const updatedTask = {...taskToToggle, reminder : !taskToToggle.reminder}
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
     console.log(updatedTask)
-    
+
     const res = await fetch(`http://localhost:2045/tasks/${id}`, {
-      method : "PUT",
-      header : {
-        'Content-type' : "application/json"
+      method: "PUT",
+      header: {
+        'Content-type': "application/json"
       },
-      body : JSON.stringify(updatedTask)
+      body: JSON.stringify(updatedTask)
     })
     const data = await res.json()
     console.log("data:")
@@ -87,21 +89,26 @@ function App() {
   }
 
   return (
-    // Java Script Syntax Extension Language
-    <div>
-      <h1 href='/' >Hello World</h1>
+    <Router>
+     {/* Java Script Syntax Extension Language */}
+      <div>
+        <h1 href='/' >Hello World</h1>
 
-      <Header onAdd = {() => setShowAddTask(!showAddTask)} showAddTask = {showAddTask}> </Header>
-      {showAddTask &&<AddTask className='container' onAdd={addTask}></AddTask>} 
-       {/* ternary operator without an else */}
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAddTask={showAddTask}> </Header>
+        {showAddTask && <AddTask className='container' onAdd={addTask}></AddTask>}
+        {/* ternary operator without an else */}
 
-      <div className='container'>
-        {tasks.length > 0 ?
-          <Tasks tasks={tasks} onDelete={deleteTask} switchTaskReminder={switchTaskReminder}></Tasks> : 'No tasks available'
-        }
+        <div className='container'>
+          {tasks.length > 0 ?
+            <Tasks tasks={tasks} onDelete={deleteTask} switchTaskReminder={switchTaskReminder}></Tasks> : 'No tasks available'
+          }
+        </div>
+        <Routes>
+          <Route path="/about" component={About}></Route>
+        </Routes>
+        <Footer></Footer>
       </div>
-      <Footer></Footer>
-    </div>
+    </Router>
   );
 }
 
